3cc1ca39bc4d91c4396998bfa68146299f0221c3
sunb <devsunb@gmail.com>
2021-09-01 22:44:00 +0900

    feature: To do list

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,81 @@
 <script>
-	export let name;
+  import Navbar from "./components/Navbar.svelte"
+  import TodoInput from "./components/TodoInput.svelte";
+  import TodoList from "./components/TodoList.svelte";
+
+  let todos = [
+    {id: 0, checked: false, text: "test1"},
+    {id: 1, checked: true, text: "test2"}
+  ]
+  $: lastId = todos[todos.length - 1]?.id || 0;
+
+  let todoInput = "";
+
+  let onHandleKeyup = e => {
+    todoInput = e.target.value;
+
+    if (e.keyCode === 13) {
+      onHandleAdd();
+    }
+  };
+  let onHandleAdd = () => {
+    if (!todoInput) {
+      return;
+    }
+
+    const newTodo = {
+      id: ++lastId,
+      checked: false,
+      text: todoInput,
+    };
+
+    todos = [...todos, newTodo];
+
+    todoInput = "";
+  };
+
+  let onHandleCheck = (id) => {
+    const index = todos.findIndex((todo) => todo.id === id);
+    todos[index]["checked"] = !todos[index]["checked"];
+  };
+
+  let onHandleRemove = (id) => {
+    todos = todos.filter((todo) => todo.id !== id);
+  };
+
+  let onHandleModify = (id, text) => {
+    const index = todos.findIndex((todo) => todo.id === id);
+    todos[index]["text"] = text;
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<section class="hero is-primary is-fullheight">
+  <div class="hero-head">
+    <Navbar/>
+  </div>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="columns is-desktop">
+        <div class="column"></div>
+        <div class="column">
+          <h1 class="title">Todo List</h1>
+          <h2 class="subtitle">Built with Svelte, Bulma</h2>
+          <TodoInput {todoInput} {onHandleKeyup} {onHandleAdd}/>
+          <TodoList {todos} {onHandleCheck} {onHandleRemove} {onHandleModify}/>
+        </div>
+        <div class="column"></div>
+      </div>
+    </div>
+  </div>
+</section>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+<style lang="scss">
+  main {
+    text-align: center;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    & > h1 {
+      color: #ff3e00
+    }
+  }
 </style>
